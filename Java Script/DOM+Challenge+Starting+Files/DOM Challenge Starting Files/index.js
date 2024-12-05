cbf2e2fc1c69b8a7682eec558d92e8fd6879f89b
Alexandre Correia <alexandremsvc@gmail.com>
2024-12-05 16:54:48 +0000

    updated

--- Java Script/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ Java Script/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,3 +1,4 @@
 document.querySelector("ul").lastElementChild.innerText = "Alex";
 document.querySelector("#list a").style.color = "red";
 document.querySelector(".btn").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
